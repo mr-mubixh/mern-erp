b120dd92b21c90c31df7deaee83aaea5f5ded2c6
mubashar.rashid@vaival.tech <mubashar.rashid@vaival.tech>
2024-04-16 10:22:52 +0500

    authController

--- Controllers/Auth.Controller.js
+++ Controllers/Auth.Controller.js
@@ -2,7 +2,6 @@ const createError = require('http-errors')
 const User = require('../Models/User.model')
 const { authSchema } = require('../helpers/validation_schema')
 const {signAccessToken} = require('../helpers/jwt_helper')
-const client = require('../helpers/init_mongodb')
 
 module.exports = {
   register: async (req, res, next) => {
@@ -44,44 +43,11 @@ module.exports = {
         throw createError.Unauthorized('Username/password not valid')
 
       const accessToken = await signAccessToken(user.id)
-      //const refreshToken = await signRefreshToken(user.id)
 
       res.send({ accessToken })
     } catch (error) {
       if (error.isJoi === true)
         return next(createError.BadRequest('Invalid Username/Password'))
-      next(error)
-    }
-  },
-
-  refreshToken: async (req, res, next) => {
-    try {
-      const { refreshToken } = req.body
-      if (!refreshToken) throw createError.BadRequest()
-      const userId = await verifyRefreshToken(refreshToken)
-
-      const accessToken = await signAccessToken(userId)
-      const refToken = await signRefreshToken(userId)
-      res.send({ accessToken: accessToken, refreshToken: refToken })
-    } catch (error) {
-      next(error)
-    }
-  },
-
-  logout: async (req, res, next) => {
-    try {
-      const { refreshToken } = req.body
-      if (!refreshToken) throw createError.BadRequest()
-      const userId = await verifyRefreshToken(refreshToken)
-      client.DEL(userId, (err, val) => {
-        if (err) {
-          console.log(err.message)
-          throw createError.InternalServerError()
-        }
-        console.log(val)
-        res.sendStatus(204)
-      })
-    } catch (error) {
       next(error)
     }
   },
